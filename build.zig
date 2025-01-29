988bb5e823cc44adfcf66ee9ffca5e8f53316b7d
Moritz Laass <moritz.laass@gmail.com>
2025-01-29 15:49:39 +0100

    moved python lib build to build_pydust.zig

--- build.zig
+++ build.zig
@@ -1,23 +1,8 @@
 const std = @import("std");
-const py = @import("./pydust.build.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    const test_step = b.step("test", "Run library tests");
-
-    const pydust = py.addPydust(b, .{
-        .test_step = test_step,
-    });
-
-    _ = pydust.addPythonModule(.{
-        .name = "termynus._lib",
-        .root_source_file = .{ .path = "src/term.zig" },
-        .limited_api = true,
-        .target = target,
-        .optimize = optimize,
-    });
 
     // Add the CLI executable
     const exe = b.addExecutable(.{
